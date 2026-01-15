8da3ff73c0f82a6d1f50423388a0c185e544a2b0
Gaganpreet Singh <149477702+gaganpreet-dev@users.noreply.github.com>
2026-01-15 14:48:56 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+//  alert(3+3) // we are using nodejs, not browser
+console.log(3 + 3)
+console.log("Gagan")
+
+
+let name = "Gagan"
+let age = 22
+let isLoggedIn = false
+let state
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value datatype =>object
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
